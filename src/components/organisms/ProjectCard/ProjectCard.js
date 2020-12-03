4fc002e8442ff21d637c74b3cd0546dcaa08e652
antonikosar <kosarantoni@gmail.com>
2020-12-03 15:47:07 +0100

    Improve close on scroll, and fix (I hope) swipe to close in ProjectCard.js

--- src/components/organisms/ProjectCard/ProjectCard.js
+++ src/components/organisms/ProjectCard/ProjectCard.js
@@ -38,7 +38,13 @@ const Wrapper = styled.div`
 
 const ContentWrapper = styled(motion.div)`
   position: relative;
-  background: white;
+  background: repeating-linear-gradient(
+  45deg,
+  #606dbc,
+  #606dbc 10px,
+  #465298 10px,
+  #465298 20px
+);
   width: 16rem;
   height: 16rem;
   margin: 0;
@@ -62,19 +68,22 @@ const ProjectCard = () => {
   const [isSelected, setIsSelected] = useState(false);
   const cardRef = useRef(null);
   const dismissScrollDistance = -50;
-  const dismissSwipeDistance = 5;
+  const dismissSwipeDistance = 20;
   const { scrollYProgress } = useViewportScroll(cardRef);
   const [y, setY] = useState(0);
 
   const checkScrollToDismiss = (pos) => {
-    setY(pos * -1);
-    if (scrollYProgress.current === 0 && pos < dismissScrollDistance) {
+    if (isSelected && scrollYProgress.current === 0) {
+      setY(pos * -5);
+    }
+    if (scrollYProgress.current === 0
+      && (pos < dismissScrollDistance || y > dismissScrollDistance * -1)) {
       setIsSelected(false);
       setY(0);
     }
   };
   const checkSwipeToDismiss = (pos) => {
-    setY(pos * 100);
+    setY(pos * 2);
     if (scrollYProgress.current === 0 && pos > dismissSwipeDistance) {
       setIsSelected(false);
     }
@@ -97,15 +106,14 @@ const ProjectCard = () => {
           isSelected={isSelected}
           ref={cardRef}
           layout
-          y={isSelected ? y : 0}
+          style={{ y }}
           animate={isSelected ? {
             borderRadius: '2rem',
           } : {
             borderRadius: '100%',
           }}
           transition={{ type: 'spring', stiffness: 300, damping: 40 }}
-          // whileTap={{ scale: 0.98 }}
-          onPan={(event, info) => checkSwipeToDismiss(info.offset.y)}
+          onPan={isSelected && ((event, info) => checkSwipeToDismiss(info.offset.y))}
           onPanEnd={() => setY(0)}
         />
       </Wrapper>
